6f5f9e70987cd37c4ede425ef51a7bae5ce47d8b
Neil Crawforth <16989539+ncrawforth@users.noreply.github.com>
2023-03-09 14:13:13 +0000

    Update installer.py

--- installer.py
+++ installer.py
@@ -4,7 +4,7 @@ def install(filename):
   s = socket.socket()
   s.connect(a)
   ssl.wrap_socket(s)
-  s.write("GET /ncrawforth/VT2040-utils/main/", filename, " HTTP/1.0\r\nHost: raw.githubusercontent.com\r\n\r\n")
+  s.write("GET /ncrawforth/VT2040-utils/main/" + filename + " HTTP/1.0\r\nHost: raw.githubusercontent.com\r\n\r\n")
   d = s.read()
   s.close()
   f = open(filename, "w")
